a0dfd50c88113976ee3883cb9932bba68ec18a80
albirex <albi.per91@gmail.com>
2017-11-19 11:04:00 +0100

    Added logging

--- TemperatureCapture.py
+++ TemperatureCapture.py
@@ -1,10 +1,13 @@
 """Module to read ds18b20 sensor value"""
+import logging
 
 class TemperatureCapture:
     """Class to check when activate relay"""
 
     @classmethod
-    def get_value(cls, sensor_id):
+    def get_value(cls, sensor_id, logger=None):
+        logger = logger or logging.getLogger(__name__)
+
         try:
             location = "/sys/bus/w1/devices/%s/w1_slave" % sensor_id
             with open(location) as sensor_file:
@@ -14,6 +17,9 @@ class TemperatureCapture:
             temperaturedata = secondline.split(" ")[9]
             temperature = float(temperaturedata[2:])
             temperature = float(temperature) / 1000
+
+            logger.info("Current temperature %s" % temperature)
+
             return temperature
         except Exception as exc:
             return 100
